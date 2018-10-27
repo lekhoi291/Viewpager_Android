52e32740055b34e36b66a7d7f2cd41b237dfd68e
lekhoi291 <35086980+lekhoi291@users.noreply.github.com>
2018-10-27 13:30:59 +0700

    update

---
+++ WorldPress/app/src/main/java/weather/usth/edu/vn/worldpress/My_Site.java
@@ -0,0 +1,15 @@
+package weather.usth.edu.vn.worldpress;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class My_Site extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_my__site, container, false);
+    }
+}
