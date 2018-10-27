c39b242d00707c07f4a8193fd7419c74101cd73e
lekhoi291 <35086980+lekhoi291@users.noreply.github.com>
2018-10-27 13:33:34 +0700

    Update WorldPress/app/src/main/java/weather/usth/edu/vn/worldpress/MainActivity.java

--- WorldPress/app/src/main/java/weather/usth/edu/vn/worldpress/MainActivity.java
+++ WorldPress/app/src/main/java/weather/usth/edu/vn/worldpress/MainActivity.java
@@ -19,9 +19,9 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
+        tabLayout.addTab(tabLayout.newTab().setText("My Site"));
+        tabLayout.addTab(tabLayout.newTab().setText("Reader"));
+        tabLayout.addTab(tabLayout.newTab().setText("Notifications"));
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
         final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
